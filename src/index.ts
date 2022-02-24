a36242dc11d8ed4bea7888b8c95b859cb73fc5b3
iosdeveloper <iosdeveloper@gmail.com>
2022-02-24 23:09:59 +0700

    handle null case with expo-go

--- src/index.ts
+++ src/index.ts
@@ -30,7 +30,7 @@ import { InAppMessage, InAppMessageAction, InAppMessageLifecycleHandlerObject } 
 import { isValidCallback, isNativeModuleLoaded } from './helpers';
 
 const RNOneSignal = NativeModules.OneSignal;
-const eventManager = new EventManager(RNOneSignal);
+const eventManager = RNOneSignal? new EventManager(RNOneSignal): null;
 
 // 0 = None, 1 = Fatal, 2 = Errors, 3 = Warnings, 4 = Info, 5 = Debug, 6 = Verbose
 export type LogLevel = 0 | 1 | 2 | 3 | 4 | 5 | 6;
